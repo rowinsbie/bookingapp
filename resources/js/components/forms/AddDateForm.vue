4f8fcc3d03b859a7fdf61c83bcfb25c4ca99bf0e
Seiki Rowins Bie <61301846+seikirowinsbie@users.noreply.github.com>
2022-05-27 18:03:12 +0800

    added a date picker

--- resources/js/components/forms/AddDateForm.vue
+++ resources/js/components/forms/AddDateForm.vue
@@ -3,7 +3,8 @@
     <div class="col-lg-8">
       <div class="form-group">
         <label for="Date">Date</label>
-        <input type="text" class="form-control" />
+          <datepicker   v-model="picked" class="form-control" />
+
       </div>
     </div>
     <div class="col-lg-4">
@@ -16,7 +17,18 @@
   </div>
 </template>
 <script>
+import Datepicker from 'vue3-datepicker'
+import { ref } from 'vue'
 export default {
+    data()
+    {
+        return {
+            picked:ref(new Date())
+        }
+    },
+    components:{
+        Datepicker
+    },
   mounted() {},
 };
 </script>
